0ee6366d1b615ece4c0f28438f95b9218fcd948d
andreasprihananto2023 <andreas.prihananto2023@student.president.ac.id>
2025-07-15 23:26:59 +0700

    Update App.py

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+from ensemble_model import EnsembleModel
 import plotly.graph_objects as go
 import plotly.express as px
 from datetime import datetime, time
